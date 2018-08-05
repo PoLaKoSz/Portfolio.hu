18801060c6f8d5ed715934867f43bedf658a7c19
PoLáKoSz <polakosz@freemail.hu>
2018-08-05 11:31:18 +0200

    Added IsValidURL function to Article endpoint

---
+++ src/Library.UnitTests/ArticleTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoLaKoSz.hu.Portfolio_hu_API;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ArticleTests
+    {
+
+        [DataTestMethod]
+        [DataRow("https://www.google.com")]
+        [DataRow("https://www.facebook.com")]
+        [DataRow("https://youtube.com")]
+        public void IsValidURL__NotPortfolioSite__ShouldReturnFalse(string url)
+        {
+            Uri address = new Uri(url);
+
+            bool actual = Article.IsValidURL(address);
+
+            Assert.AreEqual(false, actual);
+        }
+        
+        [DataTestMethod]
+        [DataRow("http://portfolio.hu")]
+        [DataRow("https://portfolio.hu")]
+        [DataRow("http://www.portfolio.hu")]
+        [DataRow("https://www.portfolio.hu")]
+        [DataRow("http://www.portfolio.hu/")]
+        [DataRow("https://www.portfolio.hu/")]
+        public void IsValidURL__PassHomepage__ShouldReturnFalse(string url)
+        {
+            Uri address = new Uri(url);
+
+            bool actual = Article.IsValidURL(address);
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("https://forum.portfolio.hu/topics/ormester-reszvenyesek-ide/13684")]
+        [DataRow("https://forum.portfolio.hu/topics/otp-reszvenyesek-ide/5224?oldal=50302")]
+        [DataRow("https://forum.portfolio.hu/topics/komoly-biralatot-kapott-a-korrupcios-helyzet-miatt-magyarorszag-brusszelbol/26999")]
+        public void IsValidURL__ForumURL__ShouldReturnFalse(string url)
+        {
+            Uri address = new Uri(url);
+
+            bool actual = Article.IsValidURL(address);
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("https://www.portfolio.hu/arfolyam-panel/BET-BUX/bux-arfolyam.html")]
+        [DataRow("https://www.portfolio.hu/arfolyam-panel/BET-OPUS/opus-reszveny-arfolyam.html")]
+        public void IsValidURL__StockURL__ShouldReturnFalse(string url)
+        {
+            Uri address = new Uri(url);
+
+            bool actual = Article.IsValidURL(address);
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("http://www.portfolio.hu/vallalatok/743-forintos-celar-erkezett-az-appeninnre-ugrik-az-arfolyam.290884.html?i=290884")]
+        [DataRow("http://portfolio.hu/hir.php?i=293194")]
+        [DataRow("http://www.portfolio.hu/hir.php?i=293194")]
+        [DataRow("https://www.portfolio.hu/befektetes/befektetesi-alapok/eletbe-lepett-az-uj-eu-s-szabaly-itt-a-kegyelemdofes-a-bankbetetek-nagy-rivalisainak.292842.html")]
+        public void IsValidURL__PassValidURL__ShouldReturnTrue(string url)
+        {
+            Uri address = new Uri(url);
+
+            bool actual = Article.IsValidURL(address);
+
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
--- src/Library/Article.cs
+++ src/Library/Article.cs
@@ -7,9 +7,17 @@ namespace PoLaKoSz.hu.Portfolio_hu_API
 {
     public class Article : EndPoint
     {
+        /// <summary>
+        /// Initialize a new Article endpoint
+        /// </summary>
+        /// <param name="articleAddress"></param>
+        /// <exception cref="ArgumentException">Occurs when the passed <see cref="Uri"/> is not valid</exception>
         public Article(Uri articleAddress)
             : base(articleAddress)
         {
+            if (!IsValidURL(articleAddress))
+                throw new ArgumentException("URL " + articleAddress.ToString() + " is not a valid article!");
+
             Middlewares.Add(new ArticleImageMiddleware());
         }
 
@@ -24,6 +32,27 @@ namespace PoLaKoSz.hu.Portfolio_hu_API
             string sourceCode = base.LoadWebpage();
 
             return ArticleDeserializer.Deserialize(sourceCode);
+        }
+
+        /// <summary>
+        /// Determinate that the given <see cref="Uri"/> is a valid article URL
+        /// </summary>
+        /// <param name="articleAddress">Questionable URL</param>
+        /// <returns></returns>
+        public static bool IsValidURL(Uri articleAddress)
+        {
+            if (!(articleAddress.Host.Equals("portfolio.hu") ||
+                articleAddress.Host.Equals("www.portfolio.hu")))
+                return false;
+
+            if (!(3 <= articleAddress.Segments.Length ||
+                articleAddress.AbsolutePath.Contains("hir.php")))
+                return false;
+
+            if (articleAddress.Segments[1].Equals("arfolyam-panel/"))
+                return false;
+
+            return true;
         }
     }
 }
